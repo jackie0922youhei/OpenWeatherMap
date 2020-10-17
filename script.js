9f934b0fdf5d79921fa3f826a2d9e9a0aa009695
西尾洋平 <jackie0922youhei@gmail.com>
2020-10-17 12:43:21 +0900

    Finish 12章

---
+++ script.js
@@ -0,0 +1,30 @@
+API_KEY = "fc349fc48fc5b8a35fc505f3e37a5ded";
+
+$(function () {
+  $("#btn").on("click", function () {
+    $.ajax({
+      url:
+        "http://api.openweathermap.org/data/2.5/weather?q=" +
+        $("#cityname").val() +
+        "&units=metric&appid=" +
+        API_KEY,
+      dataType: "jsonp",
+    })
+      .done(function (data) {
+        $("#place").text(data.name);
+        $("#temp_max").text(data.main.temp_max);
+        $("#temp_min").text(data.main.temp_min);
+        $("#humidity").text(data.main.humidity);
+        $("#speed").text(data.wind.speed);
+        $("#weather").text(data.weather[0].main);
+        $("img").attr(
+          "src",
+          "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
+        );
+        $("img").attr("alt", data.weather[0].main);
+      })
+      .fail(function (data) {
+        alert("通信に失敗しました。");
+      });
+  });
+});
